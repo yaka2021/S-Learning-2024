266885d7f78269ab5090c70f400ba7e8b4bd3413
yaka_21 <96155487+yaka2021@users.noreply.github.com>
2024-05-15 08:16:06 +0900

    Revert "Kazuki"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AppController;
-
-Route::get('/', [AppController::class, 'home']);
+Route::get('/', function () {
+    return view('welcome');
+});
