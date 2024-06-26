0285b38c0752613fa0ccabb6f2c874597bebea0c
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2024-06-26 14:19:54 +0900

    チュートリアル画面が完成。

--- resources/js/components/SignUp.jsx
+++ resources/js/components/SignUp.jsx
@@ -1,15 +1,12 @@
+import { useNavigate } from "react-router-dom"; //ページ遷移を実装するために必要
+
 export const SignUp = () => {
+    const navigate = useNavigate();
+
     return (
         <>
-        <button className="select-tutorial-button">チュートリアル</button>
-
-        <div className="difficulty">
-            <p>難易度選択</p>
-        </div>
-
-        <button className="select-level1-button">LEVEL1</button>
-        <button className="select-level2-button">LEVEL2</button>
-        <button className="select-level3-button">LEVEL3</button>
+        <main>
+        </main>
         </>
     );
 }
